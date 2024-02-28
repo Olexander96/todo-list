ed07c5066e2f742e67fea795806a00c255dd826c
Olexander96 <o.sheiko96@gmail.com>
2024-02-28 08:49:15 +0200

    completed

--- src/components/Tasks/TaskInput/TaskInput.js
+++ src/components/Tasks/TaskInput/TaskInput.js
@@ -1,35 +1,35 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./TaskInput.css";
-import styled from "styled-components";
+import styles from "./TaskInput.module.css";
+// import styled from "styled-components";
 
-const ForcmControl = styled.div`
-  margin: 0.5rem 0;
+// const ForcmControl = styled.div`
+//   margin: 0.5rem 0;
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-  color: ${props => props.invalid ? "red" : "black"};
-}
+// & label {
+//   font-weight: bold;
+//   display: block;
+//   margin-bottom: 0.5rem;
+//   color: ${props => props.invalid ? "red" : "black"};
+// }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid ${props => props.invalid ? "red" : "#ccc"};
-  background-color: ${props => props.invalid && "salmon"};
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+// & input {
+//   display: block;
+//   width: 100%;
+//   border: 1px solid ${props => props.invalid ? "red" : "#ccc"};
+//   background-color: ${props => props.invalid && "salmon"};
+//   font: inherit;
+//   line-height: 1.5rem;
+//   padding: 0 0.25rem;
+// }
 
-$ input:focus {
-  outline: none;
-  background: #c8e1e4;
-  border-color: #00358b;
-}
-`;
+// $ input:focus {
+//   outline: none;
+//   background: #c8e1e4;
+//   border-color: #00358b;
+// }
+// `;
 
 const TaskInput = (props) => {
   const [inputText, setinputText] = useState("");
@@ -53,10 +53,14 @@ const TaskInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <ForcmControl invalid={!isInputValid}>
+      {/* <ForcmControl invalid={!isInputValid}>
         <label>Задачі</label>
         <input type="text" onChange={taskInputChangeHandler} />
-      </ForcmControl>
+      </ForcmControl> */}
+      <div className={`${styles['form-control']} ${!isInputValid && styles.invalid}`}>
+        <label>Задачі</label>
+        <input type="text" onChange={taskInputChangeHandler} />
+      </div>
       <Button type="submit">Додати Задачу</Button>
     </form>
   );
